2eb0afc84d788e9fbad0e2a21afa1fb24f732502
Ilya Smirnov <giros44man@gmail.com>
2024-10-01 10:17:24 +0300

    Попытка обновить класс Lexer

--- Laba_1/Lexer.h
+++ Laba_1/Lexer.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <string>
+
+class lexer {
+	std::string file;
+	int num_str = 0;
+
+public:
+	lexer(const std::string& file_name) :file{ file_name } {};
+	lexer() = delete;
+
+	//Возвращает следующую лексему
+	std::string next_lexem() {
+
+	}
+};
