129b8bb3f4905b5f02bb67220fc5b836d2afa635
Ilya Smirnov <giros44man@gmail.com>
2024-11-14 17:39:49 +0300

    Дополнил обработчик ошибок

--- Laba_1/Parser.h
+++ Laba_1/Parser.h
@@ -2,11 +2,8 @@
 #include "Lexer.h"
 #include "Token.h"
 #include <string>
-//#include <set>
 #include "Situation.h"
 #include <fstream>
-//#include <functional>
-#include <iostream>
 
 
 class parser {
@@ -20,7 +17,7 @@ class parser {
 	std::ofstream out;
 
 public:
-	//Èìÿ ôàéëà, èìÿ ôàéëà äëÿ îøèáîê, ãðàììàòèêà, ðåæèì çàïèñè (äîçàïèñü â ôàéë îøèáîê èëè íîâàÿ çàïèñü)
+
 	parser(const std::string name_file, const std::string out_file, const std::vector <std::vector
 		<std::vector
 		<std::pair<std::string, std::string>>>>&grammar, const bool add_or_not = 0)
@@ -38,16 +35,78 @@ private:
 		return false;
 	}
 
-	void Scan(std::vector<std::vector<situation>>& D, const token& lexem) {
+	void Out_errors(const std::vector<situation>& possible_sits, const token& lexem, const bool mode) {
+		if (mode) {
+			for (situation i : possible_sits) {
+				if (grammar[i.rule][i.sub_rule][i.dot].first != "VAR" && grammar[i.rule][i.sub_rule][i.dot].first != "CONST") {
+					out << "Missing lexem '" << grammar[i.rule][i.sub_rule][i.dot].second << "' before '" << lexem.getLexem() << "'\n";
+				}
+				else if (grammar[i.rule][i.sub_rule][i.dot].first == "VAR") {
+					out << "Missing variable before '" << lexem.getLexem() << "'\n";
+				}
+				else {
+					out << "Missing constant before '" << lexem.getLexem() << "'\n";
+				}
+			}
+		}
+		else {
+			for (situation i : possible_sits) {
+				if (grammar[i.rule][i.sub_rule][i.dot].first != "VAR" && grammar[i.rule][i.sub_rule][i.dot].first != "CONST") {
+					out << "Assuming lexem '" << grammar[i.rule][i.sub_rule][i.dot].second << "' instead of '" << lexem.getLexem() << "'\n";
+				}
+				else if (grammar[i.rule][i.sub_rule][i.dot].first == "VAR") {
+					out << "Assuming variable instead of '" << lexem.getLexem() << "'\n";
+				}
+				else {
+					out << "Assuming constant instead of '" << lexem.getLexem() << "'\n";
+				}
+			}
+		}
+	}
+
+	void Handle_errors(std::vector<std::vector<situation>>& D, const token& lexem) {
+		unsigned D_size = D.size();
+		std::vector<situation> possible_situations;
+		for (situation i : D[D_size - 2]) {
+			if (i.dot != grammar[i.rule][i.sub_rule].size() &&
+				grammar[i.rule][i.sub_rule][i.dot].first != "N")
+			{
+
+				situation a(i.rule, i.sub_rule, i.dot + 1, i.sym_index);
+				if (!find(D[D_size - 1], a)) {
+					possible_situations.push_back(i);
+					D[D_size - 1].push_back(a);
+				}
+			}
+		}
+		unsigned Pars_set_size = 0;
+		while (Pars_set_size != D[D_size-1].size()) {
+			Pars_set_size = D[D_size-1].size();
+			Predict(D);
+			Complete(D);
+		}
+		D.push_back(std::vector<situation>());
+		Scan(D, lexem, 0);
+		if (D[D.size() - 1].size() != 0) {
+			Out_errors(possible_situations, lexem, 1);
+		}
+		else {
+			D.pop_back();
+			Out_errors(possible_situations, lexem, 0);
+		}
+
+	}
+
+	void Scan(std::vector<std::vector<situation>>& D, const token& lexem, bool not_error = 1) {
 		unsigned D_size = D.size();
 		unsigned S_size = D[D_size - 1].size();
 		for (situation i : D[D_size - 2]) {
 			if (i.dot != grammar[i.rule][i.sub_rule].size() &&
-				grammar[i.rule][i.sub_rule][i.dot].first != "N") // Åñëè òî÷êà íå ñòîèò â êîíöå ñèòóàöèè è òî÷êà ñòîèò ïåðåä òåðìèíàëîì
+				grammar[i.rule][i.sub_rule][i.dot].first != "N")
 			{
 				if ((grammar[i.rule][i.sub_rule][i.dot] == lexem) ||
 					((grammar[i.rule][i.sub_rule][i.dot].first == "VAR" && lexem.getType() == "VAR") ||
-						(grammar[i.rule][i.sub_rule][i.dot].first == "CONST" && lexem.getType() == "CONST"))) { // Åñëè òåðìèíàë ðàâåí ëåêñåìå òî âñòàâëÿåì åãî â ìíîæåñòâî ñèòóàöèé D[D_size-1]
+						(grammar[i.rule][i.sub_rule][i.dot].first == "CONST" && lexem.getType() == "CONST"))) { 
 					situation a(i.rule, i.sub_rule, i.dot + 1, i.sym_index);
 					if (!find(D[D_size - 1], a)) {
 						D[D_size - 1].push_back(a);
@@ -55,29 +114,9 @@ private:
 				}
 			}
 		}
-		// Åñëè íå îäíà èç ñèòóàöèé èç ïðåä. ìíîæåñòâà íå ïîäîøëà, òî çàïèñûâàþ âñå ñèòóàöèè â òåê. ìíîæåñòâî, 
-		// êîòîðûå áû â òåîðèè ìîãëè áû ïîäîéòè åñëè áû âñå áûëî êîððåêòíî
-		// È âûâîæó îøèáêó â ôàéë
-		if (S_size == D[D_size - 1].size()) {
-			for (situation i : D[D_size - 2]) {
-				if (i.dot != grammar[i.rule][i.sub_rule].size() &&
-					grammar[i.rule][i.sub_rule][i.dot].first != "N")
-				{
-					situation a(i.rule, i.sub_rule, i.dot + 1, i.sym_index);
-					if (!find(D[D_size - 1], a)) {
-						D[D_size - 1].push_back(a);
-					}
-					if (grammar[i.rule][i.sub_rule][i.dot].first != "VAR" && grammar[i.rule][i.sub_rule][i.dot].first != "CONST") {
-						out << "Assuming lexem '" << grammar[i.rule][i.sub_rule][i.dot].second << "' instead of '" << lexem.getLexem() << "'\n";
-					}
-					else if (grammar[i.rule][i.sub_rule][i.dot].first == "VAR") {
-						out << "Assuming variable instead of '" << lexem.getLexem() << "'\n";
-					}
-					else {
-						out << "Assuming constant instead of '" << lexem.getLexem() << "'\n";
-					}
-				}
-			}
+
+		if (not_error && S_size == D[D_size - 1].size()) {
+			Handle_errors(D, lexem);
 		}
 	}
 
@@ -86,15 +125,13 @@ private:
 		for (int i = 0; i < D[D_size - 1].size(); ++i) {
 			situation i_sit(D[D_size - 1][i]);
 			if (i_sit.dot != grammar[i_sit.rule][i_sit.sub_rule].size() && grammar[i_sit.rule][i_sit.sub_rule][i_sit.dot].first == "N") {
-				unsigned R_index = std::stoi(grammar[i_sit.rule][i_sit.sub_rule][i_sit.dot].second); // íîìåð ïðîäóêöèè
-				unsigned R_size = grammar[R_index].size(); // êîëè÷åñòâî ïðàâèë â ïðîäóêöèè
+				unsigned R_index = std::stoi(grammar[i_sit.rule][i_sit.sub_rule][i_sit.dot].second);
+				unsigned R_size = grammar[R_index].size();
 				for (unsigned j = 0; j < R_size; ++j) {
 					situation a(R_index, j, 0, D_size - 1);
 					if (!find(D[D_size - 1], a)) {
 						D[D_size - 1].push_back(a);
 					}
-					//show(D, D_size - 1);
-					//std::cout << j << '\n';
 				}
 			}
 		}
@@ -107,7 +144,6 @@ private:
 			if (i_sit.dot == grammar[i_sit.rule][i_sit.sub_rule].size()) {
 
 				for (unsigned j = 0; j < D[i_sit.sym_index].size(); ++j) {
-					//Åñëè òî÷êà íå â êîíöå åñëè îíà ñòîèò ïåðåä òåðìèíàëîì è åñëè ñòîèò ïåðåä íóæíûì ïðàâèëîì
 					situation j_sit(D[i_sit.sym_index][j]);
 					if (j_sit.dot != grammar[j_sit.rule][j_sit.sub_rule].size()
 						&& (grammar[j_sit.rule][j_sit.sub_rule][j_sit.dot].first == "N"
@@ -122,12 +158,12 @@ private:
 		}
 	}
 
-	/*void show(std::vector<std::vector<situation>>& D, const unsigned& i) {
-		std::cout << "---------------\n";
-		for (situation j : D[i]) {
-			std::cout << j.rule << " " << j.sub_rule << " " << j.dot << " " << j.sym_index << '\n';
-		}
-	}*/
+	///*void show(std::vector<std::vector<situation>>& D, const unsigned& i) {
+	//	std::cout << "---------------\n";
+	//	for (situation j : D[i]) {
+	//		std::cout << j.rule << " " << j.sub_rule << " " << j.dot << " " << j.sym_index << '\n';
+	//	}
+	//}*/
 
 public:
 	std::vector<std::vector<situation>> algo_Erli() {
@@ -140,23 +176,18 @@ public:
 			Predict(parse_list);
 			Complete(parse_list);
 		}
-		//show(parse_list, 0);
 
-		for (unsigned i = 1; !lex.end(); ++i) {
+		while(!lex.end()) {
 			token next_lexm(lex.next_lexem());
 			parse_list.push_back(std::vector<situation>());
 			Scan(parse_list, next_lexm);
 			Pars_set_size = 0;
-			while (Pars_set_size != parse_list[i].size()) {
-				Pars_set_size = parse_list[i].size();
+			while (Pars_set_size != parse_list[parse_list.size()-1].size()) {
+				Pars_set_size = parse_list[parse_list.size()-1].size();
 				Predict(parse_list);
 				Complete(parse_list);
 			}
-			/*show(parse_list, i);
-			std::cout << next_lexm.getLexem() << " " << i << '\n';*/
 		}
-		/*show(parse_list, parse_list.size());
-		std::cout << next_lexm.getLexem() << " " << parse_list.size() << '\n';*/
 
 		return parse_list;
 	}
--- Laba_1/main.cpp
+++ Laba_1/main.cpp
@@ -1,26 +1,7 @@
-#include "Lexer.h"
-#include <iostream>
-#include "Token.h"
-#include <string>
-//#include "hashTable.h"
-//#include <iomanip>
-//#include <fstream>
 #include "Parser.h"
 #include "Grammar_params.h"
 
 int main() {
-	//std::ofstream out("output.txt");
 	parser pr("Test_code.txt", "error.txt", my_gram);
 	pr.algo_Erli();
-	std::cout << (situation(6, 1, 0, 0) == situation(6, 1, 0, 0));
-
-	/*lexer lex("Test_code.txt", "error.txt");
-	HashTable table;
-	while (!lex.end()) {
-		token t(lex.next_lexem());
-		if (t.getType() != "UNDEFINED") {
-			table.add(t);
-			out << std::setw(5) << table.find(t) << " |" << t << '\n';
-		}
-	}*/
 }
